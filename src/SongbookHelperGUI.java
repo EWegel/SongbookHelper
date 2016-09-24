ebf46510a0b25552770ad1b94733ac618c6b524b
Daniel Warkentin <daniel.warkentin@klosebrothers.de>
2016-09-24 21:46:36 +0200

    Added first draft of GUI

---
+++ src/SongbookHelperGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Daniel on 2016-09-24.
+ */
+public class SongbookHelperGUI {
+    private JButton resetFromSongFileButton;
+    private JButton saveToSongFileButton;
+    private JList list1;
+    private JTabbedPane tabbedPane1;
+}
